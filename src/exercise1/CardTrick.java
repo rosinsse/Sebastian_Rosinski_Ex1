810623eb7daeb8c829f757ec45a25e236bdd50db
rosinsse <rosinsse@shernet.sheridancollege.ca>
2023-02-07 16:09:51 -0500

    I'm Done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -51,7 +51,7 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
-    
+        System.out.println("I'm Done!");
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
